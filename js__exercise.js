5df1e22ee6fd1ae7447dbc7225107603a813452b
serap <serapp.oral@gmail.com>
2022-05-29 21:39:58 +0300

    load

--- js__exercise.js
+++ js__exercise.js
@@ -1,47 +1,54 @@
-const rakamlar ={
-    number:{
-    1:"I",
-    2:"II",
-    3:"III",
-    4:"IV",
-    5:"V",
-    6:"VI",
-    7:"VII",
-    8:"VIII",
-    9:"IX",
-    10:"X",
-    20:"XX",
-    30:"XXX",
-    40:"XL",
-    50:"L",
-    60:"LX",
-    70:"LXX",
-    80:"LXXX",
-    90:"XC",
-    100:"C",
-    200:"CC",
-    300:"CCC",
-    400:"CD",
-    500:"D",
-    600:"DC",
-    700:"DCC",
-    800:"DCCC",
-    900:"CM",
-    1000:"M",
-    2000:"MM",
-    3000:"MMM",
+let rakamlar ={
+    
+    "1":"I",
+    "2":"II",
+    "3":"III",
+    "4":"IV",
+    "5":"V",
+    "6":"VI",
+    "7":"VII",
+    "8":"VIII",
+    "9":"IX",
+    "10":"X",
+    "20":"XX",
+    "30":"XXX",
+    "40":"XL",
+   "50":"L",
+    "60":"LX",
+    "70":"LXX",
+    "80":"LXXX",
+   "90":"XC",
+    "100":"C",
+    "200":"CC",
+    "300":"CCC",
+    "400":"CD",
+    "500":"D",
+    "600":"DC",
+    "700":"DCC",
+    "800":"DCCC",
+    "900":"CM",
+    "1000":"M",
+    "2000":"MM",
+    "3000":"MMM",
 
-}};
+};
 
 const number="12";
 
-const arr=number.split("");
+// const arr=number.split("");
 
-console.log(arr[0]);
-console.log(arr[1]);
+// console.log(arr[0]);
+// console.log(arr[1]);
 
-if(arr[0] in rakamlar){
-    let n1=arr[0];
-    
+let arr;
+let liste=[];
+if(number>0 && number<=3000){
+   arr=number.split("");
+   if(arr.length==2){
+     
+   }
+
 }
-console.log(rakamlar["1"]);
+
+console.log(rakamlar[arr[0]]);
+console.log(rakamlar[arr[1]]);
